8a083d855ba085c786f14fc32169ed6417788e8c
Lewis <lewischao9@gmail.com>
2021-04-27 23:59:36 +0800

    Add CSS and fix bugs that cause error when dealing with TBA class time

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def fetchData(url):
         secTeacher = [i.string for i in find_teacher[i].parent.parent.parent.find_all('a')]
         rearragne = list()
         for teacher in secTeacher:
-            regex = re.search(r"([a-zA-Z]*),([a-zA-Z ]*)", teacher)
+            regex = re.search(r"([a-zA-ZÀ-ú]*),([a-zA-Z .À-ú]*)", teacher)
             rearragne.append(regex.group(2) + ' ' + regex.group(1))  
         secLoc = unicodedata.normalize('NFKD',find_loc[i].next_element).strip()
         res_dict[secName] = [fullName, secTime, ", ".join(rearragne), secLoc,[]]
@@ -79,38 +79,45 @@ def processTime(courseDict_original):
 
 
     def createTime(timeString):
-        end_list = list()
-        Course = namedtuple('timeObject', ['MO', 'TU', 'WE', 'TH', 'FR'], defaults=[0, 0, 0, 0, 0])
-        ins = {'M': (0, 0), 'T': (0, 0), 'W': (0, 0), 'R': (0, 0), 'F': (0, 0)}
-        result = re.search(
-            r'(^[\.MTWRF]*)\s([\d:(APM)]*)-([\d:(APM)]*)', timeString.strip())
-        for day in result.group(1):
-            if day == '.':
-                pass
-            else:
-                ins[day] = (timeConvert(result.group(2)),
-                            timeConvert(result.group(3)))
-        # ways to shorten? map&lambda?
-            schedule = Course((ins['M'][0], ins['M'][1]), (ins['T'][0], ins['T'][1]), (
-                ins['W'][0], ins['W'][1]), (ins['R'][0], ins['R'][1]), (ins['F'][0], ins['F'][1]))
-        return schedule
+        if timeString == 'TBA':
+            return 'TBA'
+        else:
+            end_list = list()
+            Course = namedtuple('timeObject', ['MO', 'TU', 'WE', 'TH', 'FR'], defaults=[0, 0, 0, 0, 0])
+            ins = {'M': (0, 0), 'T': (0, 0), 'W': (0, 0), 'R': (0, 0), 'F': (0, 0)}
+            result = re.search(
+                r'(^[\.MTWRF]*)\s([\d:(APM)]*)-([\d:(APM)]*)', timeString.strip())
+            for day in result.group(1):
+                if day == '.':
+                    pass
+                else:
+                    ins[day] = (timeConvert(result.group(2)),
+                                timeConvert(result.group(3)))
+            # ways to shorten? map&lambda?
+                schedule = Course((ins['M'][0], ins['M'][1]), (ins['T'][0], ins['T'][1]), (
+                    ins['W'][0], ins['W'][1]), (ins['R'][0], ins['R'][1]), (ins['F'][0], ins['F'][1]))
+            return schedule
+
 
 
     def formatTime(inputString):
-    
-        end_list = list()
-        temp_dict = {'M':'MO', 'T':'TU', 'W':'WE', 'R':'TH', 'F':'FR'}
-        temp_dict2 = {'MO':'20210906', 'TU':'20210907', 'WE':'20210908', 'TH':'20210909', 'FR':'20210910'}
-        firstDay, firstDate = '', ''
-        daylist = list()
-        for weekday in inputString.strip()[:8]:
-            if weekday in ['M', 'T', 'W', 'R', 'F']:
-                daylist.append(temp_dict[weekday]) 
-        firstDay = daylist[0]
-        firstDate = temp_dict2[firstDay]
-        firstDayTime = getattr(createTime(inputString), firstDay)
-        result = (",".join(daylist),firstDate+'T'+firstDayTime[0],firstDate+'T'+firstDayTime[1])
-        return result
+        if createTime(inputString) != 'TBA':
+            end_list = list()
+            temp_dict = {'M':'MO', 'T':'TU', 'W':'WE', 'R':'TH', 'F':'FR'}
+            temp_dict2 = {'MO':'20210906', 'TU':'20210907', 'WE':'20210908', 'TH':'20210909', 'FR':'20210910'}
+            firstDay, firstDate = '', ''
+            daylist = list()
+            for weekday in inputString.strip()[:8]:
+                if weekday in ['M', 'T', 'W', 'R', 'F']:
+                    daylist.append(temp_dict[weekday]) 
+            firstDay = daylist[0]
+            firstDate = temp_dict2[firstDay]
+            firstDayTime = getattr(createTime(inputString), firstDay)
+            result = (",".join(daylist),firstDate+'T'+firstDayTime[0],firstDate+'T'+firstDayTime[1])
+            return result
+        else:
+            return 'TBA'
+
 
     def dict_to_link(courseDict_final):
         """
@@ -119,20 +126,23 @@ def processTime(courseDict_original):
         location={LOCATION. ‘ ’ if TBD}&text={COURSE ID}&details=Instructor: {INSTRUCTOR} <br/> Classroom: {LOCATION}
         """
         resultList = list()
-
         for section, val in courseDict_final.items():
             mylist = re.findall(r'(.*?);', courseDict_original[section][1])
             tempList = list()
             for timing in mylist:
                 time_final = formatTime(timing)
-                link = ("https://calendar.google.com/calendar/u/0/r/eventedit?dates={}/{}"
-                "&ctz=America/New_York&recur=RRULE:FREQ=WEEKLY;UNTIL=20211210T235900;WKST=SU;BYDAY={}&"
-                "location={}&text={}&details=Instructor: {} <br/> Classroom: {}")
-                startTime, endTime, classDay = time_final[1], time_final[2], time_final[0]
-                instructor, location, courseId = val[2], val[3], val[0]
-                location_inlink = '' if location == 'TBA' else location # UPDATE THIS!
-                result = link.format(startTime, endTime, classDay, location_inlink, courseId, instructor, location)
-                tempList.append(result)
+                if time_final != 'TBA': 
+                    link = ("https://calendar.google.com/calendar/u/0/r/eventedit?dates={}/{}"
+                    "&ctz=America/New_York&recur=RRULE:FREQ=WEEKLY;UNTIL=20211210T235900;WKST=SU;BYDAY={}&"
+                    "location={}&text={}&details=Instructor: {} <br/> Classroom: {}")
+                    startTime, endTime, classDay = time_final[1], time_final[2], time_final[0]
+                    instructor, location, courseId = val[2], val[3], val[0]
+                    instructor = val[2] if instructor != '' else 'STAFF'
+                    location_inlink = '' if location == 'TBA' else location # UPDATE THIS!
+                    result = link.format(startTime, endTime, classDay, location_inlink, courseId, instructor, location)
+                    tempList.append(result)
+                else:
+                    tempList.append(time_final)
             resultList.append(tempList)
 
         i = 0
@@ -150,33 +160,33 @@ crossDict = get_crosslisting("Catalog - Wesleyan University - 20210424.html")
 
 @app.route("/", methods=["POST","GET"])
 def newSearch():
-    try:
-        if request.method == "POST":
-            courseName = request.form["course"].upper()
-            if courseName in crossDict:
-                courseName = crossDict[courseName]
-            if courseName in catalog:
-                session["courseName"] = courseName
-                return redirect(url_for("result"))
-            else:
-                flash('Looks like you entered a wrong class number!', 'info')
-                return redirect(url_for("newSearch"))
+    
+    if request.method == "POST":
+        courseName = request.form["course"].upper()
+        if courseName in crossDict:
+            courseName = crossDict[courseName]
+        if courseName in catalog:
+            session["courseName"] = courseName
+            return redirect(url_for("result"))
         else:
-            return render_template('index.html')
-    except:
-        return render_template('errorPage.html') 
+            flash('Looks like you entered a wrong class number!', 'info')
+            return redirect(url_for("newSearch"))
+    else:
+        return render_template('index.html')
+    
+    # return render_template('errorPage.html') 
 
 @app.route('/result', methods=["GET"])
 def result():
-    try:
-        if "courseName" in session:
-            courseName = session["courseName"]
-            return render_template('results.html', catalog = catalog, data=processTime(fetchData(catalog[courseName])))
-        else:
-            return redirect(url_for("newSearch"))
-    except:
-        return render_template('errorPage.html') 
+
+    if "courseName" in session:
+        courseName = session["courseName"]
+        return render_template('results.html', catalog = catalog, data=processTime(fetchData(catalog[courseName])))
+    else:
+        return redirect(url_for("newSearch"))
+
+        # return render_template('errorPage.html') 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
